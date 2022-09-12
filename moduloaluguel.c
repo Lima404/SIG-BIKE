e7e2d423741f456df3ba3f6e4da9ea10aee389dc
gabriel <gabriel.lima2307@uol.com.br>
2022-09-12 19:10:06 -0300

    update aluguel

--- moduloaluguel.c
+++ moduloaluguel.c
@@ -24,9 +24,11 @@ char menu_aluguel(void){
     printf("=====================Menu Aluguel====================\n");
     printf("===                                               ===\n");
     printf("===              1.Cadastro Aluguel               ===\n");
-    printf("===              2.Lista de Aluguel               ===\n");
-    printf("===              3.Apagar Aluguel                 ===\n");
-    printf("===              4.Editar Aluguel                 ===\n");
+    printf("===              2.Apagar Aluguel                 ===\n");
+    printf("===              3.Editar Aluguel                 ===\n");
+    printf("===              4.Lista de Veiculos disponiveis: ===\n");
+    printf("===              5.Lista de veiculos alugados:    ===\n");  
+    printf("===              6.Lista de Alugueis atrazado:    ===\n");
     printf("===              0.Voltar                         ===\n");
     printf("===                                               ===\n");
     printf("===                                               ===\n");
