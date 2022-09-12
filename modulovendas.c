4abf0750cadca9dc14a12c9465d40b78d17e3805
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-09-12 14:54:53 -0300

    Update modulovendas.c

--- modulovendas.c
+++ modulovendas.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <locale.h>
+#include "modulovendas.h"
 
 char menu_vendas(void){
     char op;
@@ -9,32 +10,38 @@ char menu_vendas(void){
     system("clear||cls");
     setlocale(LC_ALL,"");
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
-    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-    printf("///            Projeto Locadora de bicicletas e patins elétricos            ///\n");
-    printf("///              Developed by @lima_g99 @erick_bzrs - 2022.2                ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("/// = = = = = = = = = = = = = = Menu de Vendas = = = = = = = = = = = = = = =///\n");
-    printf("///                                                                         ///\n");
-    printf("///            1. Cadastro de Vendas:                                       ///\n");
-    printf("///            2. Lista de Vendas:                                          ///\n");
-    printf("///            3. Deletar Vendas:                                           ///\n");
-    printf("///            4. Editar Vendas:                                            ///\n");
-    printf("///            0. Voltar:                                                   ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            Escolha por onde você quer navegar:                          ///\n");
+    printf("=====================================================\n");
+    printf("=====================================================\n");
+    printf("-----------------------------------------------------\n");
+    printf("░██████╗██╗░██████╗░░░░░░░██████╗░██╗██╗░░██╗███████╗\n");
+    printf("██╔════╝██║██╔════╝░░░░░░░██╔══██╗██║██║░██╔╝██╔════╝\n");
+    printf("╚█████╗░██║██║░░██╗░█████╗██████╦╝██║█████═╝░█████╗░░\n");
+    printf("░╚═══██╗██║██║░░╚██╗╚════╝██╔══██╗██║██╔═██╗░██╔══╝░░\n");
+    printf("██████╔╝██║╚██████╔╝░░░░░░██████╦╝██║██║░╚██╗███████╗\n");
+    printf("╚═════╝░╚═╝░╚═════╝░░░░░░░╚═════╝░╚═╝╚═╝░░╚═╝╚══════╝\n");
+    printf("-----------------------------------------------------\n");
+    printf("=====================================================\n");
+    printf("=====================================================\n");
+    printf("======SISTEMAS DE BICICLETAS E PATINS ELÉTRICOS======\n");
+    printf("======================Menu Vendas====================\n");
+    printf("===                                               ===\n");
+    printf("===              1.Cadastro Venda                 ===\n");
+    printf("===              2.Lista Venda                    ===\n");
+    printf("===              3.Apagar Venda                   ===\n");
+    printf("===              4.Editar Venda                   ===\n");
+    printf("===              0.Voltar                         ===\n");
+    printf("===                                               ===\n");
+    printf("===                                               ===\n");
+    printf("===                                               ===\n");
+    printf("=====================================================\n");
     scanf("%c", &op);
     getchar();
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
+    printf("===                                               ===\n");
+    printf("===                                               ===\n");
+    printf("=====================================================\n");
+    printf
     printf("\t\t\t<<< ... Aguarde ... >>>\n");
     sleep(1);
     return op;
+
 }
