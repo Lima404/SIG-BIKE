ebee383aec95e768fdce380772ecf5a23d7decc6
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-09-12 13:29:58 -0300

    Update Cadastro de Usuário e menu novo de usuário

--- modulocliente.c
+++ modulocliente.c
@@ -3,43 +3,100 @@
 #include <unistd.h>
 #include <locale.h>
 #include "modulocliente.h"
-//char cadastro_usuario(void);
-//har lista_usuario(void);
-//char apagar_usuario(void);
-//char editar_usuario(void);
 
-char menu_usuario(void){
+
+
+void lista_usuario(void);
+char apagar_usuario(void);
+char editar_usuario(void);
+void cadastro_usuario(void);
+char menu_usuario(void); 
+
+
+char  menu_usuario(void){
     char op;
 
     system("clear||cls");
     setlocale(LC_ALL,"");
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
-    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-    printf("///            Projeto Locadora de bicicletas e patins elétricos            ///\n");
-    printf("///              Developed by @lima_g99 @erick_bzrs - 2022.2                ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("/// = = = = = = = = = = = = = = Menu de Usuário = = = = = = = = = = = = = = ///\n");
-    printf("///                                                                         ///\n");
-    printf("///            1. Cadastro de Usuário:                                      ///\n");
-    printf("///            2. Lista de Usuários:                                        ///\n");
-    printf("///            3. Apagar Usuários:                                          ///\n");
-    printf("///            4. Editar Usuários:                                          ///\n");
-    printf("///            5. Procurar Usuário:                                         ///\n");
-    printf("///            0. Voltar:                                                   ///\n");
-    printf("///                                                                         ///\n");
+    printf("=====================================================\n");
+    printf("=====================================================\n");
+    printf("-----------------------------------------------------\n");
+    printf("░██████╗██╗░██████╗░░░░░░░██████╗░██╗██╗░░██╗███████╗\n");
+    printf("██╔════╝██║██╔════╝░░░░░░░██╔══██╗██║██║░██╔╝██╔════╝\n");
+    printf("╚█████╗░██║██║░░██╗░█████╗██████╦╝██║█████═╝░█████╗░░\n");
+    printf("░╚═══██╗██║██║░░╚██╗╚════╝██╔══██╗██║██╔═██╗░██╔══╝░░\n");
+    printf("██████╔╝██║╚██████╔╝░░░░░░██████╦╝██║██║░╚██╗███████╗\n");
+    printf("╚═════╝░╚═╝░╚═════╝░░░░░░░╚═════╝░╚═╝╚═╝░░╚═╝╚══════╝\n");
+    printf("-----------------------------------------------------\n");
+    printf("=====================================================\n");
+    printf("=====================================================\n");
+    printf("======SISTEMAS DE BICICLETAS E PATINS ELÉTRICOS======\n");
+    printf("=====================Menu Cliente====================\n");
+    printf("===                                               ===\n");
+    printf("===              1.Cadastro Usuário               ===\n");
+    printf("===              2.Lista de Usuários              ===\n");
+    printf("===              3.Apagar Usuários                ===\n");
+    printf("===              4.Editar Usuários                ===\n");
+    printf("===              5.Procurar Usuário               ===\n");
+    printf("===              0.Voltar                         ===\n");
+    printf("===                                               ===\n");
+    printf("===                                               ===\n");
+    printf("===                                               ===\n");
+    printf("=====================================================\n");
     scanf("%c", &op);
     getchar();
-    printf("///                                                                         ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
+    printf("===                                               ===\n");
+    printf("===                                               ===\n");
+    printf("=====================================================\n");
+    printf
     printf("\t\t\t<<< ... Aguarde ... >>>\n");
     sleep(1);
     return op;
+
+}
+
+
+void cadastro_usuario(void){
+    
+    char nome [20];
+    char CPF [11];
+    char Email [20];
+    char data_de_nascimento[20];
+
+    system("clear||cls");
+    setlocale(LC_ALL,"");
+    printf("\n");
+    printf("=====================================================\n");
+    printf("=====================================================\n");
+    printf("-----------------------------------------------------\n");
+    printf("░██████╗██╗░██████╗░░░░░░░██████╗░██╗██╗░░██╗███████╗\n");
+    printf("██╔════╝██║██╔════╝░░░░░░░██╔══██╗██║██║░██╔╝██╔════╝\n");
+    printf("╚█████╗░██║██║░░██╗░█████╗██████╦╝██║█████═╝░█████╗░░\n");
+    printf("░╚═══██╗██║██║░░╚██╗╚════╝██╔══██╗██║██╔═██╗░██╔══╝░░\n");
+    printf("██████╔╝██║╚██████╔╝░░░░░░██████╦╝██║██║░╚██╗███████╗\n");
+    printf("╚═════╝░╚═╝░╚═════╝░░░░░░░╚═════╝░╚═╝╚═╝░░╚═╝╚══════╝\n");
+    printf("-----------------------------------------------------\n");
+    printf("=====================================================\n");
+    printf("=====================================================\n");
+    printf("======SISTEMAS DE BICICLETAS E PATINS ELÉTRICOS======\n");
+    printf("================Menu Cadastro de Cliente=============\n");
+    printf("===                                               ===\n");
+    printf(" | Digite seu nome: ");
+    scanf("%[A-Z a-z]", nome);
+    getchar(); 
+    printf(" | Digite seu CPF: ");
+    scanf("%[0-9.,..,-]", CPF); 
+    getchar();
+    printf(" | Digite seu E-mail: ");   
+    scanf("%[A-Z a-z.,0-9.,@.,_.,..,-.,/]", Email);
+    getchar();
+    printf(" | Digite sua data de nascimento: ");
+    scanf("%[A-Z]", data_de_nascimento);
+    getchar();
+    printf("=== Usuário foi cadastrado no sistema!!           ===\n");
+    printf("===                                               ===\n");
+    printf("===                                               ===\n");
+    printf(" Press ENTER to exit...");
+    getchar();
 }
--- modulocliente.h
+++ modulocliente.h
@@ -1,4 +1,5 @@
-char cadastro_usuario(void);
-char lista_usuario(void);
-char apagar_usuario(void);
-char editar_usuario(void);
+void cadastro_usuario(void);
+void lista_usuario(void);
+void apagar_usuario(void);
+void editar_usuario(void);
+char menu_usuario(void); 
