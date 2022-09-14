e5f67aeac02b27309d191c74b1d977be40061ea4
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-09-14 09:06:30 -0300

    Criação do menu cadastro de aluguel

--- MAIN/moduloaluguel.c
+++ MAIN/moduloaluguel.c
@@ -40,3 +40,47 @@ char menu_aluguel(void){
     sleep(1);
     return op;
 }
+
+char menu_cadastro_aluguel(void){
+
+    //Declarar variáveis que precisaremos usar no cadastro de aluguel
+
+    system("clear||cls");
+    printf("\n");
+    printf("=====================================================\n");
+    printf("=====================================================\n");
+    printf("-----------------------------------------------------\n");
+    printf("░██████╗██╗░██████╗░░░░░░░██████╗░██╗██╗░░██╗███████╗\n");
+    printf("██╔════╝██║██╔════╝░░░░░░░██╔══██╗██║██║░██╔╝██╔════╝\n");
+    printf("╚█████╗░██║██║░░██╗░█████╗██████╦╝██║█████═╝░█████╗░░\n");
+    printf("░╚═══██╗██║██║░░╚██╗╚════╝██╔══██╗██║██╔═██╗░██╔══╝░░\n");
+    printf("██████╔╝██║╚██████╔╝░░░░░░██████╦╝██║██║░╚██╗███████╗\n");
+    printf("╚═════╝░╚═╝░╚═════╝░░░░░░░╚═════╝░╚═╝╚═╝░░╚═╝╚══════╝\n");
+    printf("-----------------------------------------------------\n");
+    printf("-----------------------------------------------------\n");
+    printf("=====================================================\n");
+    printf("=====================================================\n");
+    printf("======SISTEMAS DE BICICLETAS E PATINS ELÉTRICOS======\n");
+    printf("================Menu Aluguel - Cadastro==============\n");
+    printf("===                                               ===\n");
+    printf(" | Digite seu nome: \n"); //copiei do menu de cadastro de clientes, depois só trocar as variáveis para Cadastro de aluguel
+    scanf("%[A-Z a-z]", nome);
+    getchar();
+    printf(" | Digite seu CPF: \n");
+    scanf("%[0-9.,..,-]", CPF); 
+    getchar();
+    printf(" | Digite seu Telefone: \n");   
+    scanf("%[0-9.,-.,(.,).,/]", Telefone);
+    getchar();
+    printf(" | Digite sua data de nascimento: \n");
+    scanf("%[0-9.,/]", data_de_nascimento);
+    getchar();
+    printf(" | Digite seu endereco: \n");
+    scanf("%[A-Z., 0-9.,]", Endereco);
+    getchar();
+    printf("=== Usuario foi cadastrado no sistema!!           ===\n");
+    printf("===                                               ===\n");
+    printf("===                                               ===\n");
+    printf(" Press ENTER to exit...");
+    getchar();
+}
