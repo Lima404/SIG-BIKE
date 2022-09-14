15fc257dbb7b45561015f2491f9987dcaceee89b
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-09-14 09:00:32 -0300

    Update moduloaluguel.h

--- MAIN/moduloaluguel.h
+++ MAIN/moduloaluguel.h
@@ -1,6 +1,6 @@
 char menu_cadastro_aluguel(void);
 char menu_lista_aluguel(void);
 char menu_editar_aluguel(void);
-char excluir_aluguel(void);
+char menu_excluir_aluguel(void);
 char lista_atrasados(void);
 char menu_aluguel(void);
