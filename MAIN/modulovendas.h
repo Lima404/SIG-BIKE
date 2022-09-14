1636b3751920f8368b5443bf17e90f3076143b4f
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-09-14 09:51:02 -0300

    Alteração de variáveis

--- MAIN/modulovendas.h
+++ MAIN/modulovendas.h
@@ -1,5 +1,5 @@
 char menu_cadastro_produto(void);
-char lista_produto(void);
-char editar_produto(void);
-char apagar_produto(void);
+char menu_lista_produto(void);
+char menu_editar_produto(void);
+char menu_apagar_produto(void);
 char menu_vendas(void);
