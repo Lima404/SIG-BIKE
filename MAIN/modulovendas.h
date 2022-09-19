e871365002f1070da2ce47220289ae603b058e48
ErickBezerrar <102674727+ErickBezerrar@users.noreply.github.com>
2022-09-19 15:02:09 -0300

    main: corrigindo vendas

--- MAIN/modulovendas.h
+++ MAIN/modulovendas.h
@@ -1,6 +1,6 @@
-void menu_nav_venda(void);
+void menu_nav_venda(char esc);
 char menu_venda(void);
 void menu_nova_venda(void);
 void menu_lista_venda(void);
-char menu_editar_vendas(void);
-void menu_devolucao_produto(void);
+char menu_editar_venda(void);
+char menu_devolucao_produto(void);
