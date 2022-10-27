8834029fbc92b0f309893708cb97348f7c5480ca
gabriel <gabriel.lima2307@uol.com.br>
2022-10-27 10:45:47 -0300

    Adicionado structs modulo_cliente

--- MAIN/modulocliente.h
+++ MAIN/modulocliente.h
@@ -1,7 +1,29 @@
 void menu_nav_cliente(void);
 char menu_cliente(void);
-void menu_cadastro_cliente(void);
+// void menu_cadastro_cliente(void);
 char menu_lista_cliente(void);
-char menu_apagar_cliente(void);
+// char menu_apagar_cliente(void);
 char menu_editar_cliente(void);
 char menu_procurar_cliente(void);
+
+//  CADASTRO CLIENTE
+
+typedef struct cadastro Cadastro;
+struct cadastro{
+    char nome [20];
+    char cpf [11];
+    char telefone [20];
+    char endereco [40];
+    int dd, mm, aa;
+};
+
+Cadastro* cadastroCliente( );
+
+// APAGAR CLIENTE
+
+typedef struct apagar Apagar;
+struct apagar{
+    char cpf [11];
+};
+
+Apagar* apagarCliente( );
