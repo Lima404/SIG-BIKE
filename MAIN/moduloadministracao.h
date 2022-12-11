0a9d4cc8d6724235b106a592471bec19b94b5f64
Gabriel lima <gabriel.lima2307@uol.com.br>
2022-12-11 03:14:03 -0300

    algumas mudanças

--- MAIN/moduloadministracao.h
+++ MAIN/moduloadministracao.h
@@ -2,12 +2,26 @@ void menu_nav_adm(void);
 char menu_adm(void);
 
 
+typedef struct aCliente NoCliente;
+struct aCliente{
+
+    char nome [81];
+    char cpf [12];
+    char telefone [21];
+    char endereco [141];
+    int dd, mm, aa;
+    char status;
+    NoCliente* prox;
+};
+
+// USUÁRIO
 char menu_relatorios_cliente(void);
 void nav_relatorio_cliente(void);
 void R_lista_cliente(void);
-char R_cliente_alfa(void);
+NoCliente* R_cliente_alfa(void);
+void exibe_alfa(NoCliente*);
 
-
+// VEICULO
 char menu_relatorio_veiculo(void);
 void nav_relatorio_veiculo(void);
 void R_lista_veiculo(void);
